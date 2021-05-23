81cae7eb4684391e51caf4f8c31793ceb2cac33c
linjn <443082762@qq.com>
2021-05-23 23:00:12 +0800

    优化请求的发送。

--- week2/src/main/java/MyHttpClient.java
+++ week2/src/main/java/MyHttpClient.java
@@ -5,8 +5,12 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
+ * 第二周作业
  * 使用 httpClient 访问 api
  *
  * @author LinJn
@@ -17,11 +21,16 @@ public class MyHttpClient {
     private final CloseableHttpClient httpClient;
 
     private MyHttpClient() {
-        httpClient = HttpClientBuilder.create().build();
+        httpClient = HttpClientBuilder
+                .create()
+                .build();
     }
 
-    public String getAsString(String url) throws IOException {
+    public String getAsString(String url, List<Map<String, String>> headers) throws IOException {
         HttpGet httpGet = new HttpGet(url);
+        for (Map<String, String> header : headers) {
+            httpGet.addHeader(header.get("name"), header.get("value"));
+        }
         try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
             return EntityUtils.toString(response.getEntity());
         }
@@ -29,7 +38,7 @@ public class MyHttpClient {
 
     public static void main(String[] args) throws IOException {
         MyHttpClient httpClient = new MyHttpClient();
-        String asString = httpClient.getAsString("http://localhost:8082");
+        String asString = httpClient.getAsString("http://localhost:8082", Collections.emptyList());
         System.out.println(asString);
     }
 }
