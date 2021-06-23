2f5bb585a3d8806076063d7dbba719de8fa9c20c
linjn <443082762@qq.com>
2021-06-23 11:15:11 +0800

    增加第八周的作业

---
+++ week8/src/main/java/Homework01.java
@@ -0,0 +1,13 @@
+/**
+ * 作业1
+ *
+ * @author LinJn
+ * @since 2021/6/23 11:14
+ */
+public class Homework01 {
+
+    public static void main(String[] args) {
+        System.out.println("作业1");
+    }
+
+}
