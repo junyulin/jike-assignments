af2c38ef7c89b305a70a52d1d13719c06fcf39bd
linjn <443082762@qq.com>
2021-06-23 11:26:00 +0800

    完成开发

--- week8/src/main/java/Homework01.java
+++ week8/src/main/java/Homework01.java
@@ -9,6 +9,7 @@ public class Homework01 {
     public static void main(String[] args) {
         System.out.println("作业8");
         System.out.println("继续开发新模块");
+        System.out.println("完成开发");
     }
 
 }
